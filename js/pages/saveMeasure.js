178e7507bc14e323b0dd81e5593e4070cf7c1770
David Lopez <dhelaman@hotmail.com>
2017-05-18 12:32:16 -0400

    removed returned data

--- js/pages/saveMeasure.js
+++ js/pages/saveMeasure.js
@@ -5,19 +5,6 @@
 $(document).ready(function() {
     $("#btnSaveItem").click(function() {
         var mesDesc = $("#txtNewItem").val();
-        $.post("queries/saveMeasure.php", {mesDesc: mesDesc}, function (data, textStatus) {
-            console.info(data);
-            console.info(textStatus);
-        });
-        /*$('#createStation').on('shown.bs.modal', function (e) {
-         modalLoaded = true;
-         google.maps.event.trigger($('#googleMap2')[0], 'resize');
-         myMap();
-         });
-         $('#createStation').on('hidden.bs.modal', function (e) {
-         modalLoaded = false;
-         myMap();
-         });
-         */
+        $.post("queries/saveMeasure.php", {mesDesc: mesDesc});
     });
 });
--- queries/saveMeasure.php
+++ queries/saveMeasure.php
@@ -29,16 +29,5 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-/*
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Desc: " . $row["description"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-*/
-
 $conn->close();
 ?>
