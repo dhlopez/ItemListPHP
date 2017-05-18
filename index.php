b2775bbed36feb36fee4e9cf22f545bf78e6bb49
David Lopez <dhelaman@hotmail.com>
2017-05-17 20:21:15 -0400

    item and measure modal and db changes

--- index.php
+++ index.php
@@ -9,10 +9,22 @@
 <?php
 echo "My first PHP script!";
 ?>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    Name: <input type="text" name="fname">
-    <input type="submit" class="btn btn-success">
-</form>
+<div class="container content">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Name: <input type="text" name="fname">
+        <input type="submit" class="btn btn-success">
+    </form>
+
+    <!-- Buttons trigger for modals -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#newMeasure">
+        New Measure
+    </button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#newItem">
+        New Item
+    </button>
+
+</div>
+
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -26,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<div class="modal fade" tabindex="-1" role="dialog" id="myModal">
+<div class="modal fade" tabindex="-1" role="dialog" id="newItem">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -56,10 +68,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-    New Item
-</button>
+<div class="modal fade" tabindex="-1" role="dialog" id="newMeasure">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">New Measure</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="newItem col-xs-12 form-group">
+                        <label class="control-label">Measure</label>
+                        <input type="text" class="form-control" placeholder="Ex. Kilo, Bag, Box, etc.">
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save Item</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 
 <?php
     include 'footer.php';
